cc62ab64d7f4d6b98610c571cc558c0bc1d60f47
Aither <cheese736@hotmail.com>
2023-02-16 19:04:57 +0800

    add a modal to anchor: aboutus in header.hbs

--- public/scripts/filter.js
+++ public/scripts/filter.js
@@ -1,5 +1,6 @@
 const checkboxes = document.querySelectorAll('.weekdayCheckbox')
 const tableContent = document.querySelectorAll('tr .days')
+const partyNameKeyword = document.querySelector('#partyName')
 
 function dayFilter(e) {
   const value = e.target.value

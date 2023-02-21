1003c629fd9a47897208784e503f6b792bd9458a
Aither <cheese736@hotmail.com>
2023-02-22 02:56:55 +0800

    self review migrations

--- migrations/20230112172955-add-category-to-discussion.js
+++ migrations/20230112172955-add-category-to-discussion.js
@@ -3,13 +3,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Discussions', 'categoryId', {
+    await queryInterface.addColumn('Discussions', 'category_id', {
       type: Sequelize.INTEGER,
       defaultValue: 0,
     })
   },
 
   async down(queryInterface, _Sequelize) {
-    await queryInterface.removeColumn('Discussions', 'categoryId')
+    await queryInterface.removeColumn('Discussions', 'category_id')
   },
 }

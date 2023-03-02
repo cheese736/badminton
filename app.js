f2b02f4846ebb9f6b488b723c8a2e005c18ee2ab
Aither <cheese736@hotmail.com>
2023-03-02 17:57:12 +0800

    modify variable: port

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const path = require('path')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 }
-const port = 3000
+const port = process.env.PORT || 3000
 const routes = require('./routes')
 app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
